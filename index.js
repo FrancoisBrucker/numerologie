c6fbd17012b9cc79ef30e8c497efb529bd5e7fa0
François Brucker <francois.brucker@gmail.com>
2021-10-13 17:15:45 +0200

    console.log

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-
-    
-    console.log("-------")
-    console.log(req.url);
-    console.log("========")
-    console.log(req.method);
-    console.log("========")
-    console.log(req.httpVersion);
-    console.log("========")
-    console.log(req.headers);
+    console.log(req.url)
     
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    res.setHeader('Content-Type', 'text/html');
+
+    fichier = fs.readFileSync("./index.html", {encoding:'utf8'})
+    res.end(fichier);
 });
 
 server.listen(port, hostname, () => {

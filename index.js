2f77ad13cbc787986847fd1d6942206f82fcd40a
François Brucker <francois.brucker@gmail.com>
2021-10-13 18:42:33 +0200

    ajout express et refonte

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 const path = require('path')
-const http = require('http');
-const fs = require('fs');
+
+const express = require('express')
+const app = express()
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    console.log(req.url)
+app.use(function (req, res, next) {
+    date = new Date(Date.now())
+    console.log('Time:', date.toLocaleDateString(), date.toLocaleTimeString(), "; url :", req.url);
+    next(); // sans cette ligne on ne pourra pas poursuivre.
+})
 
-    if (req.url === "/") {
-        fichier = path.join(__dirname, "./index.html")        
-    } else {
-        fichier = path.join(__dirname,  req.url)
-    }
-    console.log("fichier : " + fichier)
-    if (fs.existsSync(fichier)) {
-        res.writeHead(200,  {'Content-Type': 'text/html'})
-    
-        fichier = fs.readFileSync(fichier, {encoding:'utf8'})
-        res.end(fichier);
-    }
-    else {
-    res.writeHead(404,  {'Content-Type': 'text/plain'})
-    res.end();
-    }
-});
+app.use("/static", express.static(path.join(__dirname, '/static')))
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.get('/', (req, res) => {
+    res.redirect(301, '/static/index.html')
+})
+
+
+app.use(function (req, res) {
+    console.log("et c'est le 404 : " + req.url);
+
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/html');
+
+    res.end("<html><head><title>la quatre cent quatre</title></head><body><h1>Et c'est la 404.</h1><img  src=\"https://www.leblogauto.com/wp-content/uploads/2020/04/Peugeot-404-1.jpg\" /></body></html>");
+
+})
+
+app.listen(port, hostname);
+console.log(`Server running at http://${hostname}:${port}/`);

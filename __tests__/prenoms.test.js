28becc3f3b39191819a42bf34f33b4d682543b04
François Brucker <francois.brucker@gmail.com>
2021-10-20 22:22:14 +0200

    env test

---
+++ __tests__/prenoms.test.js
@@ -0,0 +1,32 @@
+const request = require('supertest');
+
+process.env.NODE_ENV = 'test'
+
+const db = require("../db")
+const app = require('../app');
+const { stream } = require('npmlog');
+
+beforeEach(async () => {
+    await db.sequelize.sync({force: true})
+    for (i=1 ; i <10 ; i +=1) {
+        await db.model.Signification.create({
+            message: i,
+            nombre: i,
+        })
+    }
+})
+
+test('GET /prénom?valeur=toto pas de prénom', (done) => {
+    request(app)
+        .get(encodeURI("/prénom") + "?valeur=toto")
+        .expect(200)
+        .expect((res) => {
+            expect(res.body).toEqual({"prénom":"toto","chiffre":4,"message":"4"})
+        })
+        .end((err, res) => {
+            if (err) {
+                return done(err)
+            }
+            return done()
+        })
+})
--- db.js
+++ db.js
@@ -1,18 +1,24 @@
 const { Sequelize, DataTypes } = require('sequelize');
 path = require('path')
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: path.join(__dirname, 'db.sqlite')
-});
+let env = process.env.NODE_ENV || 'dev'
+
+if (env == 'test') {
+  sequelize = new Sequelize('sqlite::memory:');
+} else {
+  sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: path.join(__dirname, 'db.sqlite')
+  });
+}
 
 signification = require("./models/signification")
 prenoms = require("./models/prenoms")
 
 module.exports = {
-    sequelize: sequelize,
-    model: {
-        Signification: signification(sequelize),
-        Prenoms: prenoms(sequelize),
-    }
+  sequelize: sequelize,
+  model: {
+    Signification: signification(sequelize),
+    Prenoms: prenoms(sequelize),
+  }
 }
